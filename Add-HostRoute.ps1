879099af8dec3d47529ab6bfc5b2a159ca601e87
Savenkov Ivan <vip-ic@mail.ru>
2023-10-03 21:33:24 +0300

    Сделал рефакторинг имён переменных.

--- Add-HostRoute.ps1
+++ Add-HostRoute.ps1
@@ -26,7 +26,7 @@
   Скрипт не генерит возвращаемые объекты.
 
   .NOTES
-  Версия: 0.2.1
+  Версия: 0.2.2
   Автор: @ViPiC
 #>
 
@@ -47,131 +47,131 @@ Param (
 
 $MaskDelim = "/";
 $maxCIDRv4 = 32; $maxCIDRv6 = 128;
-$isPASSv4 = $true; $isPASSv6 = $true;
+$isPassIPv4 = $true; $isPassIPv6 = $true;
 # Получаем информацию об ip адресах шлюза
-$GwIPv4s = @(); $GwIPv6s = @();
-if ($Gateway -and ($isPASSv4 -or $isPASSv6)) {
+$GatewaysIPv4 = @(); $GatewaysIPv6 = @();
+if ($Gateway -and ($isPassIPv4 -or $isPassIPv6)) {
     $List = @($Gateway);
     if ([IPAddress]::TryParse($List[0], [ref][IPAddress]::Loopback)) {
-        if (($List[0].IndexOf(".") -ge 0) -and $isPASSv4) { $GwIPv4s = @($List[0]); };
-        if (($List[0].IndexOf(":") -ge 0) -and $isPASSv6) { $GwIPv6s = @($List[0]); };
+        if (($List[0].IndexOf(".") -ge 0) -and $isPassIPv4) { $GatewaysIPv4 = @($List[0]); };
+        if (($List[0].IndexOf(":") -ge 0) -and $isPassIPv6) { $GatewaysIPv6 = @($List[0]); };
     }
     else {
         $Resolve = (Resolve-DnsName -Name $List[0] -ErrorAction "Ignore" | Where-Object "Section" -EQ "Answer");
-        if ($Resolve -and $isPASSv4) { foreach ($GwIPv4 in $Resolve.IP4Address) { $GwIPv4s += $GwIPv4; }; };
-        if ($Resolve -and $isPASSv6) { foreach ($GwIPv6 in $Resolve.IP6Address) { $GwIPv6s += $GwIPv6; }; };
+        if ($Resolve -and $isPassIPv4) { foreach ($GatewayIPv4 in $Resolve.IP4Address) { $GatewaysIPv4 += $GatewayIPv4; }; };
+        if ($Resolve -and $isPassIPv6) { foreach ($GatewayIPv6 in $Resolve.IP6Address) { $GatewaysIPv6 += $GatewayIPv6; }; };
     };
-    $isPASSv4 = ($GwIPv4s.Count -gt 0);
-    $isPASSv6 = ($GwIPv6s.Count -gt 0);
+    $isPassIPv4 = ($GatewaysIPv4.Count -gt 0);
+    $isPassIPv6 = ($GatewaysIPv6.Count -gt 0);
 };
-# Получаем информацию об ip адресах проверяемого узла
-$ChkIPv4s = @(); $ChkIPv6s = @();
-if ($Check -and ($isPASSv4 -or $isPASSv6)) {
+# Получаем информацию об ip адресах проверяемого узла и проверяем доступность
+$ChecksIPv4 = @(); $ChecksIPv6 = @();
+if ($Check -and ($isPassIPv4 -or $isPassIPv6)) {
     $List = @($Check);
     if ([IPAddress]::TryParse($List[0], [ref][IPAddress]::Loopback)) {
-        if (($List[0].IndexOf(".") -ge 0) -and $isPASSv4) { $ChkIPv4s = @($List[0]); };
-        if (($List[0].IndexOf(":") -ge 0) -and $isPASSv6) { $ChkIPv6s = @($List[0]); };
+        if (($List[0].IndexOf(".") -ge 0) -and $isPassIPv4) { $ChecksIPv4 = @($List[0]); };
+        if (($List[0].IndexOf(":") -ge 0) -and $isPassIPv6) { $ChecksIPv6 = @($List[0]); };
     }
     else {
         $Resolve = (Resolve-DnsName -Name $List[0] -ErrorAction "Ignore" | Where-Object "Section" -EQ "Answer");
-        if ($Resolve -and $isPASSv4) { foreach ($ChkIPv4 in $Resolve.IP4Address) { $ChkIPv4s += $ChkIPv4; }; };
-        if ($Resolve -and $isPASSv6) { foreach ($ChkIPv6 in $Resolve.IP6Address) { $ChkIPv6s += $ChkIPv6; }; };
+        if ($Resolve -and $isPassIPv4) { foreach ($CheckIPv4 in $Resolve.IP4Address) { $ChecksIPv4 += $CheckIPv4; }; };
+        if ($Resolve -and $isPassIPv6) { foreach ($CheckIPv6 in $Resolve.IP6Address) { $ChecksIPv6 += $CheckIPv6; }; };
     };
-    $isPASSv4 = (($ChkIPv4s.Count -gt 0) -and (Test-Connection -ComputerName $ChkIPv4s -Quiet -ErrorAction "Ignore"));
-    $isPASSv6 = (($ChkIPv6s.Count -gt 0) -and (Test-Connection -ComputerName $ChkIPv6s -Quiet -ErrorAction "Ignore"));
+    $isPassIPv4 = (($ChecksIPv4.Count -gt 0) -and (Test-Connection -ComputerName $ChecksIPv4 -Quiet -ErrorAction "Ignore"));
+    $isPassIPv6 = (($ChecksIPv6.Count -gt 0) -and (Test-Connection -ComputerName $ChecksIPv6 -Quiet -ErrorAction "Ignore"));
 };
 # Получаем информацию об ip адресах хоста назначения
-$DstIPv4s = @(); $DstIPv6s = @();
-if ($Destination -and ($isPASSv4 -or $isPASSv6)) {
+$DestinationsIPv4 = @(); $DestinationsIPv6 = @();
+if ($Destination -and ($isPassIPv4 -or $isPassIPv6)) {
     $List = $Destination.Split($MaskDelim);
     if ([IPAddress]::TryParse($List[0], [ref][IPAddress]::Loopback)) {
-        if (($List[0].IndexOf(".") -ge 0) -and $isPASSv4) { $DstIPv4s = @($List[0]); };
-        if (($List[0].IndexOf(":") -ge 0) -and $isPASSv6) { $DstIPv6s = @($List[0]); };
+        if (($List[0].IndexOf(".") -ge 0) -and $isPassIPv4) { $DestinationsIPv4 = @($List[0]); };
+        if (($List[0].IndexOf(":") -ge 0) -and $isPassIPv6) { $DestinationsIPv6 = @($List[0]); };
     }
     else {
         $Resolve = (Resolve-DnsName -Name $List[0] -ErrorAction "Ignore" | Where-Object "Section" -EQ "Answer");
-        if ($Resolve -and $isPASSv4) { foreach ($DstIPv4 in $Resolve.IP4Address) { $DstIPv4s += $DstIPv4; }; };
-        if ($Resolve -and $isPASSv6) { foreach ($DstIPv6 in $Resolve.IP6Address) { $DstIPv6s += $DstIPv6; }; };
+        if ($Resolve -and $isPassIPv4) { foreach ($DestinationIPv4 in $Resolve.IP4Address) { $DestinationsIPv4 += $DestinationIPv4; }; };
+        if ($Resolve -and $isPassIPv6) { foreach ($DestinationIPv6 in $Resolve.IP6Address) { $DestinationsIPv6 += $DestinationIPv6; }; };
     };
-    $isPASSv4 = ($DstIPv4s.Count -gt 0);
-    $isPASSv6 = ($DstIPv6s.Count -gt 0);
+    $isPassIPv4 = ($DestinationsIPv4.Count -gt 0);
+    $isPassIPv6 = ($DestinationsIPv6.Count -gt 0);
 };
 # Прнобразовываем ip адреса хоста назначения в адреса подсетей
-$DstNETv4s = @(); $DstNETv6s = @();
-if ($Destination -and ($isPASSv4 -or $isPASSv6)) {
+$DestinationsNETv4 = @(); $DestinationsNETv6 = @();
+if ($Destination -and ($isPassIPv4 -or $isPassIPv6)) {
     $List = $Destination.Split($MaskDelim);
     $CIDRv4 = if ($List.Count -eq 2) { [Math]::Max(0, [Math]::Min($List[1] * 1, $maxCIDRv4)) } else { $maxCIDRv4 };
-    foreach ($DstIPv4 in $DstIPv4s) {
-        $IPv4 = [IPAddress]::Parse($DstIPv4);
+    foreach ($DestinationIPv4 in $DestinationsIPv4) {
+        $IPv4 = [IPAddress]::Parse($DestinationIPv4);
         $MASKv4 = [IPAddress]([Math]::Pow(2, $maxCIDRv4) - 1 -bxor [Math]::Pow(2, ($maxCIDRv4 - $CIDRv4)) - 1);
         $NETv4 = [IPAddress]($IPv4.Address -band $MASKv4.Address);
-        $DstNETv4 = $NETv4.IPAddressToString + $MaskDelim + $CIDRv4;
-        if (-not($DstNETv4 -in $DstNETv4s)) { $DstNETv4s += $DstNETv4; };
+        $DestinationNETv4 = $NETv4.IPAddressToString + $MaskDelim + $CIDRv4;
+        if (-not($DestinationNETv4 -in $DestinationsNETv4)) { $DestinationsNETv4 += $DestinationNETv4; };
     };
     $CIDRv6 = if ($List.Count -eq 2) { [Math]::Max(0, [Math]::Min($List[1] * 1, $maxCIDRv6)) } else { $maxCIDRv6 };
-    foreach ($DstIPv6 in $DstIPv6s) {
-        $IPv6 = [IPAddress]::Parse($DstIPv6);
+    foreach ($DestinationIPv6 in $DestinationsIPv6) {
+        $IPv6 = [IPAddress]::Parse($DestinationIPv6);
         $NETv6 = $IPv6;
-        $DstNETv6 = $NETv6.IPAddressToString + $MaskDelim + $CIDRv6;
-        if (-not($DstNETv6 -in $DstNETv6s)) { $DstNETv6s += $DstNETv6; };
+        $DestinationNETv6 = $NETv6.IPAddressToString + $MaskDelim + $CIDRv6;
+        if (-not($DestinationNETv6 -in $DestinationsNETv6)) { $DestinationsNETv6 += $DestinationNETv6; };
     };
 };
 # Определяем интерфейсы для шлюзов
-$IfIDv4s = @(); $IfIDv6s = @();
-if ($Gateway -and ($isPASSv4 -or $isPASSv6)) {
-    foreach ($GwIPv4 in $GwIPv4s) {
-        $Find = Find-NetRoute -RemoteIPAddress $GwIPv4 -ErrorAction "Ignore";
-        $IfIDv4 = if ($Find) { $Find[0].InterfaceIndex } else { $null };
-        $IfIDv4s += $IfIDv4;
+$InterfacesIDXv4 = @(); $InterfacesIDXv6 = @();
+if ($Gateway -and ($isPassIPv4 -or $isPassIPv6)) {
+    foreach ($GatewayIPv4 in $GatewaysIPv4) {
+        $Find = Find-NetRoute -RemoteIPAddress $GatewayIPv4 -ErrorAction "Ignore";
+        $InterfaceIDXv4 = if ($Find) { $Find[0].InterfaceIndex } else { $null };
+        $InterfacesIDXv4 += $InterfaceIDXv4;
     };
-    foreach ($GwIPv6 in $GwIPv6s) {
-        $Find = Find-NetRoute -RemoteIPAddress $GwIPv6 -ErrorAction "Ignore";
-        $IfIDv6 = if ($Find) { $Find[0].InterfaceIndex } else { $null };
-        $IfIDv6s += $IfIDv6;
+    foreach ($GatewayIPv6 in $GatewaysIPv6) {
+        $Find = Find-NetRoute -RemoteIPAddress $GatewayIPv6 -ErrorAction "Ignore";
+        $InterfaceIDXv6 = if ($Find) { $Find[0].InterfaceIndex } else { $null };
+        $InterfacesIDXv6 += $InterfaceIDXv6;
     };
 };
 # Удаляем устаревшие маршруты
-$NetRoutes = (Get-NetRoute -RouteMetric $Metric -PolicyStore "ActiveStore" -ErrorAction "Ignore" | Where-Object "NextHop" -NotIn "::","0.0.0.0");
+$NetRoutes = (Get-NetRoute -RouteMetric $Metric -PolicyStore "ActiveStore" -ErrorAction "Ignore" | Where-Object "NextHop" -NotIn "::", "0.0.0.0");
 if (-not($NetRoutes)) { $NetRoutes = @(); };
 foreach ($NetRoute in $NetRoutes) {
-    $isPASSv4 = $false;
-    for ($i = 0; ($i -lt $GwIPv4s.Count) -and -not($isPASSv4); $i++) {
-        if (($NetRoute.NextHop -eq $GwIPv4s[$i]) -and ($NetRoute.InterfaceIndex -eq $IfIDv4s[$i])) {
-            for ($j = 0; ($j -lt $DstNETv4s.Count) -and -not($isPASSv4); $j++) {
-                if ($NetRoute.DestinationPrefix -eq $DstNETv4s[$j]) { $isPASSv4 = $true; };
+    $isPassIPv4 = $false;
+    for ($i = 0; ($i -lt $GatewaysIPv4.Count) -and -not($isPassIPv4); $i++) {
+        if (($NetRoute.NextHop -eq $GatewaysIPv4[$i]) -and ($NetRoute.InterfaceIndex -eq $InterfacesIDXv4[$i])) {
+            for ($j = 0; ($j -lt $DestinationsNETv4.Count) -and -not($isPassIPv4); $j++) {
+                if ($NetRoute.DestinationPrefix -eq $DestinationsNETv4[$j]) { $isPassIPv4 = $true; };
             };
         };
     };
-    $isPASSv6 = $false;
-    for ($i = 0; ($i -lt $GwIPv6s.Count) -and -not($isPASSv6); $i++) {
-        if (($NetRoute.NextHop -eq $GwIPv6s[$i]) -and ($NetRoute.InterfaceIndex -eq $IfIDv6s[$i])) {
-            for ($j = 0; ($j -lt $DstNETv6s.Count) -and -not($isPASSv6); $j++) {
-                if ($NetRoute.DestinationPrefix -eq $DstNETv6s[$j]) { $isPASSv6 = $true; };
+    $isPassIPv6 = $false;
+    for ($i = 0; ($i -lt $GatewaysIPv6.Count) -and -not($isPassIPv6); $i++) {
+        if (($NetRoute.NextHop -eq $GatewaysIPv6[$i]) -and ($NetRoute.InterfaceIndex -eq $InterfacesIDXv6[$i])) {
+            for ($j = 0; ($j -lt $DestinationsNETv6.Count) -and -not($isPassIPv6); $j++) {
+                if ($NetRoute.DestinationPrefix -eq $DestinationsNETv6[$j]) { $isPassIPv6 = $true; };
             };
         };
     };
-    if (-not($isPASSv4 -or $isPASSv6)) { Remove-NetRoute -InputObject $NetRoute -Confirm:$false; };
+    if (-not($isPassIPv4 -or $isPassIPv6)) { Remove-NetRoute -InputObject $NetRoute -Confirm:$false; };
 };
 # Добавляем новые маршруты
-for ($i = 0; $i -lt $GwIPv4s.Count; $i++) {
-    for ($j = 0; $j -lt $DstNETv4s.Count; $j++) {
-        $isPASSv4 = $IfIDv4s[$i];
+for ($i = 0; $i -lt $GatewaysIPv4.Count; $i++) {
+    for ($j = 0; $j -lt $DestinationsNETv4.Count; $j++) {
+        $isPassIPv4 = $InterfacesIDXv4[$i];
         foreach ($NetRoute in $NetRoutes) {
-            if (($NetRoute.NextHop -eq $GwIPv4s[$i]) -and ($NetRoute.InterfaceIndex -eq $IfIDv4s[$i])) {
-                if ($NetRoute.DestinationPrefix -eq $DstNETv4s[$j]) { $isPASSv4 = $false; };
+            if (($NetRoute.NextHop -eq $GatewaysIPv4[$i]) -and ($NetRoute.InterfaceIndex -eq $InterfacesIDXv4[$i])) {
+                if ($NetRoute.DestinationPrefix -eq $DestinationsNETv4[$j]) { $isPassIPv4 = $false; };
             };
         };
-        if ($isPASSv4) { $NetRoute = New-NetRoute -DestinationPrefix $DstNETv4s[$j] -NextHop $GwIPv4s[$i] -InterfaceIndex $IfIDv4s[$i] -RouteMetric $Metric -PolicyStore "ActiveStore"; };
+        if ($isPassIPv4) { $NetRoute = New-NetRoute -DestinationPrefix $DestinationsNETv4[$j] -NextHop $GatewaysIPv4[$i] -InterfaceIndex $InterfacesIDXv4[$i] -RouteMetric $Metric -PolicyStore "ActiveStore"; };
     };
 };
-for ($i = 0; $i -lt $GwIPv6s.Count; $i++) {
-    for ($j = 0; $j -lt $DstNETv6s.Count; $j++) {
-        $isPASSv6 = $IfIDv6s[$i];
+for ($i = 0; $i -lt $GatewaysIPv6.Count; $i++) {
+    for ($j = 0; $j -lt $DestinationsNETv6.Count; $j++) {
+        $isPassIPv6 = $InterfacesIDXv6[$i];
         foreach ($NetRoute in $NetRoutes) {
-            if (($NetRoute.NextHop -eq $GwIPv6s[$i]) -and ($NetRoute.InterfaceIndex -eq $IfIDv6s[$i])) {
-                if ($NetRoute.DestinationPrefix -eq $DstNETv6s[$j]) { $isPASSv6 = $false; };
+            if (($NetRoute.NextHop -eq $GatewaysIPv6[$i]) -and ($NetRoute.InterfaceIndex -eq $InterfacesIDXv6[$i])) {
+                if ($NetRoute.DestinationPrefix -eq $DestinationsNETv6[$j]) { $isPassIPv6 = $false; };
             };
         };
-        if ($isPASSv6) { $NetRoute = New-NetRoute -DestinationPrefix $DstNETv6s[$j] -NextHop $GwIPv6s[$i] -InterfaceIndex $IfIDv6s[$i] -RouteMetric $Metric -PolicyStore "ActiveStore"; };
+        if ($isPassIPv6) { $NetRoute = New-NetRoute -DestinationPrefix $DestinationsNETv6[$j] -NextHop $GatewaysIPv6[$i] -InterfaceIndex $InterfacesIDXv6[$i] -RouteMetric $Metric -PolicyStore "ActiveStore"; };
     };
 };
