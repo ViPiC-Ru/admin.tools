2292eda35c7729c77d47283cb5397b0c2d975bbf
Savenkov Ivan <vip-ic@mail.ru>
2023-10-04 21:30:35 +0300

    Переписал на совместную проверку по IPv4 и IPv6.

--- Sync-ComputerState.ps1
+++ Sync-ComputerState.ps1
@@ -34,16 +34,16 @@
   Скрипт не генерит возвращаемые объекты.
 
   .NOTES
-  Версия: 0.1.0
+  Версия: 0.1.1
   Автор: @ViPiC
 #>
 
 [CmdletBinding()]
 Param (
-    [string]$Source = "",
-    [string]$Target = "",
-    [string]$Check = "",
-    [string]$MAC = "",
+    [string]$Source,
+    [string]$Target,
+    [string]$Check,
+    [string]$MAC,
 
     [ValidateRange(1, 99)]
     [int]$Try = 3,
@@ -52,35 +52,76 @@ Param (
     [int]$Port = 9
 );
 
-if ((-not $Check) -or (Test-Connection -ComputerName $Check -Quiet -Count $Try)) {
-    if ((-not $Source) -or (Test-Connection -ComputerName $Source -Quiet -Count $Try)) {
-        if ((-not $Target) -or (Test-Connection -ComputerName $Target -Quiet -Count $Try)) {
-        }
-        else {
-            # Включаем целевой компьютер
-            if ($MAC) {
-                $BroadcastProxy = [System.Net.IPAddress]::Broadcast;
-                $chainSync = [byte[]](, 0xFF * 6); # цепочка синфронизации
-                $MacAddress = $MAC -split "-" | ForEach-Object { [byte]("0x" + $PSItem) };
-                $MagicPacket = $chainSync + $MacAddress * 16;
-                $UdpClient = New-Object "System.Net.Sockets.UdpClient";
-                $UdpClient.Connect($BroadcastProxy, $Port);
-                for ($i = 0; $i -lt $Try; $i++) {
-                    $UdpClient.Send($MagicPacket, $MagicPacket.Length) | Out-Null;
-                };
-                $UdpClient.Close();
-            };
-        };
+$isCheckOnline = $true; $isSourceOnline = $true; $isTargetOnline = $true;
+# Получаем информацию об ip адресах дополнительного компьютера и проверяем доступность
+$ChecksIPv4 = @(); $ChecksIPv6 = @();
+if ($Check) {
+    $List = @($Check);
+    if ([IPAddress]::TryParse($List[0], [ref][IPAddress]::Loopback)) {
+        if (($List[0].IndexOf(".") -ge 0)) { $ChecksIPv4 = @($List[0]); };
+        if (($List[0].IndexOf(":") -ge 0)) { $ChecksIPv6 = @($List[0]); };
     }
     else {
-        if ((-not $Target) -or (Test-Connection -ComputerName $Target -Quiet -Count $Try)) {
-            # Выключаем целевой компьютер
-            if ($Target) {
-                Stop-Computer -ComputerName $Target -Force;
-            }
-            else {
-                Stop-Computer -Force;
-            };
+        $Resolve = (Resolve-DnsName -Name $List[0] -ErrorAction "Ignore" | Where-Object "Section" -EQ "Answer");
+        if ($Resolve) { foreach ($CheckIPv4 in $Resolve.IP4Address) { $ChecksIPv4 += $CheckIPv4; }; };
+        if ($Resolve) { foreach ($CheckIPv6 in $Resolve.IP6Address) { $ChecksIPv6 += $CheckIPv6; }; };
+    };
+    $isPassIPv4 = (($ChecksIPv4.Count -gt 0) -and (Test-Connection -ComputerName $ChecksIPv4 -Count $Try -Quiet -ErrorAction "Ignore"));
+    $isPassIPv6 = (($ChecksIPv6.Count -gt 0) -and (Test-Connection -ComputerName $ChecksIPv6 -Count $Try -Quiet -ErrorAction "Ignore"));
+    $isCheckOnline = ($isPassIPv4 -or $isPassIPv6);
+};
+# Получаем информацию об ip адресах исходного компьютера и проверяем доступность
+$SourcesIPv4 = @(); $SourcesIPv6 = @();
+if ($Source -and $isCheckOnline) {
+    $List = @($Source);
+    if ([IPAddress]::TryParse($List[0], [ref][IPAddress]::Loopback)) {
+        if (($List[0].IndexOf(".") -ge 0)) { $SourcesIPv4 = @($List[0]); };
+        if (($List[0].IndexOf(":") -ge 0)) { $SourcesIPv4 = @($List[0]); };
+    }
+    else {
+        $Resolve = (Resolve-DnsName -Name $List[0] -ErrorAction "Ignore" | Where-Object "Section" -EQ "Answer");
+        if ($Resolve) { foreach ($SourceIPv4 in $Resolve.IP4Address) { $SourcesIPv4 += $SourceIPv4; }; };
+        if ($Resolve) { foreach ($SourceIPv6 in $Resolve.IP6Address) { $SourcesIPv6 += $SourceIPv6; }; };
+    };
+    $isPassIPv4 = (($SourcesIPv4.Count -gt 0) -and (Test-Connection -ComputerName $SourcesIPv4 -Count $Try -Quiet -ErrorAction "Ignore"));
+    $isPassIPv6 = (($SourcesIPv6.Count -gt 0) -and (Test-Connection -ComputerName $SourcesIPv6 -Count $Try -Quiet -ErrorAction "Ignore"));
+    $isSourceOnline = ($isPassIPv4 -or $isPassIPv6);
+};
+# Получаем информацию об ip адресах целевого компьютера и проверяем доступность
+$TargetsIPv4 = @(); $TargetsIPv6 = @();
+if ($Target -and $isCheckOnline) {
+    $List = @($Target);
+    if ([IPAddress]::TryParse($List[0], [ref][IPAddress]::Loopback)) {
+        if (($List[0].IndexOf(".") -ge 0)) { $TargetsIPv4 = @($List[0]); };
+        if (($List[0].IndexOf(":") -ge 0)) { $TargetsIPv4 = @($List[0]); };
+    }
+    else {
+        $Resolve = (Resolve-DnsName -Name $List[0] -ErrorAction "Ignore" | Where-Object "Section" -EQ "Answer");
+        if ($Resolve) { foreach ($TargetIPv4 in $Resolve.IP4Address) { $TargetsIPv4 += $TargetIPv4; }; };
+        if ($Resolve) { foreach ($TargetIPv6 in $Resolve.IP6Address) { $TargetsIPv6 += $TargetIPv6; }; };
+    };
+    $isPassIPv4 = (($TargetsIPv4.Count -gt 0) -and (Test-Connection -ComputerName $TargetsIPv4 -Count $Try -Quiet -ErrorAction "Ignore"));
+    $isPassIPv6 = (($TargetsIPv6.Count -gt 0) -and (Test-Connection -ComputerName $TargetsIPv6 -Count $Try -Quiet -ErrorAction "Ignore"));
+    $isTargetOnline = ($isPassIPv4 -or $isPassIPv6);
+};
+# Выполняем действие над целевым компьютером
+if ($isCheckOnline) {
+    # Включаем целевой компьютер
+    if (-not($isTargetOnline) -and $isSourceOnline -and $MAC) {
+        $BroadcastProxy = [System.Net.IPAddress]::Broadcast;
+        $chainSync = [byte[]](, 0xFF * 6); # цепочка синфронизации
+        $MacAddress = $MAC -split "-" | ForEach-Object { [byte]("0x" + $PSItem) };
+        $MagicPacket = $chainSync + $MacAddress * 16;
+        $UdpClient = New-Object "System.Net.Sockets.UdpClient";
+        $UdpClient.Connect($BroadcastProxy, $Port);
+        for ($i = 0; $i -lt $Try; $i++) {
+            $UdpClient.Send($MagicPacket, $MagicPacket.Length) | Out-Null;
         };
+        $UdpClient.Close();
+    };
+    # Выключаем целевой компьютер
+    if (-not($isSourceOnline) -and $isTargetOnline) {
+        if ($Target) { Stop-Computer -ComputerName $Target -Force; }
+        else { Stop-Computer -Force; };
     };
 };
